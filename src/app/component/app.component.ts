836cfb48d9e31986f20db20ddf9b7bf645049f7c
garche <79264096+garche@users.noreply.github.com>
2024-03-18 20:01:33 +0500

    routerEvent

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { LoggerService } from '../service/logger.service';
+import {
+    NavigationCancel,
+    NavigationEnd,
+    NavigationError,
+    NavigationStart,
+    Router,
+    RouterEvent
+} from "@angular/router";
+import { BehaviorSubject, tap } from "rxjs";
 
 @Component({
     selector: 'app-root',
@@ -7,9 +16,32 @@ import { LoggerService } from '../service/logger.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    public routingProcess: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
+
     constructor(
-        private _loggerService: LoggerService
+        private _loggerService: LoggerService,
+        private _router: Router,
     ) {
         _loggerService.writeLog('AppComponent constructor')
+
+        this._router.events
+            .pipe(
+                tap((event: any) => {
+                    if(event instanceof NavigationStart) {
+                        this.routingProcess.next(true)
+                    }
+                    if(event instanceof NavigationEnd) {
+                        this.routingProcess.next(false)
+                    }
+                    if(event instanceof NavigationError) {
+                        this.routingProcess.next(false)
+                    }
+                    if(event instanceof NavigationCancel) {
+                        this.routingProcess.next(false)
+                    }
+                })
+            )
+            .subscribe()
     }
 }
